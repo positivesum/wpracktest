1edebee77abdbe2ec594cf94b612754c0248f401
AlexanderY <1@.(none)>
2010-11-30 16:19:16 +0200

    phprack runner adding

---
+++ phprack.php
@@ -0,0 +1,7 @@
+<?php
+// this param is mandatory, others are optional
+$phpRackConfig = array(
+    'dir' => 'rack-tests',
+);
+// absolute path to the bootstrap script on your server
+include 'phpRack/bootstrap.php';
